4d8888fef3a95989f0afb9721e8567f95ad47f44
eaglerose4931 <57387921+eaglerose4931@users.noreply.github.com>
2019-11-04 23:17:19 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,32 @@
+#coding=utf8
+import requests
+import itchat
+
+KEY = '8edce3ce905a4c1dbb965e6b35c3834d'
+
+def get_response(msg):
+    apiUrl = 'http://www.tuling123.com/openapi/api'
+    data = {
+        'key': KEY,
+        'info': msg,
+        'userid': 'wechat-robot',
+    }
+    try:
+        r = requests.post(apiUrl, data=data).json()
+        return r.get('text')
+    except:
+        return
+
+@itchat.msg_register(itchat.content.TEXT)
+def tuling_reply(msg):
+    # default reply
+    defaultReply = 'In the dream, auto reply'
+    # tuling reply
+    reply = get_response(msg['Text'])
+
+    print(msg['Text'])
+    return defaultReply
+
+#hot start
+itchat.auto_login(hotReload=True)
+itchat.run()
